451324938fa67dd9f7e139a79743dc2654fed83c
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-07-03 11:59:55 +0900

    study: sharedPreferenceチュート

--- app/src/main/java/com/example/livedata/MainActivity.kt
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.livedata
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("プリファレンス", Context.MODE_PRIVATE)
+
+        val savedText = sharedPref.getString("name", "daiki")
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+        editText.setText(savedText)
+
+        val button = findViewById<Button>(R.id.save_button)
+        button.setOnClickListener {
+            val inputText = editText.text.toString()
+            sharedPref.edit().putString("name", inputText).apply()
+        }
     }
 }
