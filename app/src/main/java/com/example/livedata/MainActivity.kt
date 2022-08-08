bf9aead9e044c0ff05c627d4a4a91b8e85693066
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-08-08 21:27:10 +0900

    feat: 高階関数とラムダ式の省略

--- app/src/main/java/com/example/livedata/MainActivity.kt
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 高階関数
+        fun hyouji(n: Int) {
+            println(n)
+        }
+
+        val arr = arrayOf(1, 2, 3, 4)
+//        arr.forEach(::hyouji)
+        // 匿名関数　ラムダ
+        arr.forEach { println(it) }
     }
 }
