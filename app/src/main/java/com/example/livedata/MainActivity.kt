011d5809bc5ef519b97bb6080de0d3347b56371b
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-08-09 22:09:12 +0900

    feat: 可変長引数

--- app/src/main/java/com/example/livedata/MainActivity.kt
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -7,5 +7,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun allSum(vararg values: Int): Int {
+            var result = 0
+            for (value in values) {
+                result += value
+            }
+            return result
+        }
+        println(allSum(1, 2, 4, 6, 7))
+
+        fun allSum2(vararg values: Int): Int {
+            var result = 0
+            for (value in values) {
+                result += value
+            }
+            return result
+        }
+
+        var arr = intArrayOf(2, 4, 6, 8)
+        //スプレッド演算子を使うことで配列を可変長引数として渡せる
+        println(allSum2(*arr))
+
     }
 }
