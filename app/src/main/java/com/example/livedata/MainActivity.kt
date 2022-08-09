52a167564c1c3c510872fc0b16f2a0c908d06eb3
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2022-08-10 08:55:39 +0900

    feat: ラムダ式とreturnのラベル

--- app/src/main/java/com/example/livedata/MainActivity.kt
+++ app/src/main/java/com/example/livedata/MainActivity.kt
@@ -13,9 +13,23 @@ class MainActivity : AppCompatActivity() {
             println(n)
         }
 
-        val arr = arrayOf(1, 2, 3, 4)
+//        val arr = arrayOf(1, 2, 3, 4)
 //        arr.forEach(::hyouji)
         // 匿名関数　ラムダ
-        arr.forEach { println(it) }
+//        arr.forEach { println(it) }
+
+        //ラムダ式の中でreturn呼び出すと、ラムダ式ではなく直上の関数を抜けてしまう.この場合onCreateを終わらせている。
+//        val arr = arrayOf(1, 2, 3, 4)
+//        arr.forEach {
+//            if (it == 3) return
+//            println(it) // 1 2で終わってしまう
+//        }
+
+        val arr2 = arrayOf(1, 2, 3, 4)
+        arr2.forEach {
+            if (it == 3) return@forEach
+            println(it) // 3の時だけラムダ式を終了させている。そのため1 2 4 終了しましたと表示される。
+        }
+        println("終了しました")
     }
 }
